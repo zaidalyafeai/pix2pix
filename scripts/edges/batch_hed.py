161e4520cd4f9282a889329745f9abd99d8555f0
Zaid Alyafeai <alyafey22@gmail.com>
2018-07-10 14:23:48 +0300

    Update batch_hed.py

--- scripts/edges/batch_hed.py
+++ scripts/edges/batch_hed.py
@@ -68,7 +68,7 @@ for i in range(nImgs):
     net.forward()
     fuse = net.blobs['sigmoid-fuse'].data[0][0, :, :]
     # get rid of the border
-    fuse = fuse[border:-border, border:-border]
+    #fuse = fuse[border:-border, border:-border]
     # save hed file to the disk
     name, ext = os.path.splitext(imgList[i])
     sio.savemat(os.path.join(args.hed_mat_dir, name + '.mat'), {'predict':fuse})
